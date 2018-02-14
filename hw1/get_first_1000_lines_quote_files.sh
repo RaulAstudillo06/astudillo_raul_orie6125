45bd7580e652880149ed0d7bf84f7dff24e0426b
RaulAstudillo06 <astudillo0606@gmail.com>
2018-02-13 19:13:11 -0500

    Remaining files.

---
+++ hw1/get_first_1000_lines_quote_files.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+filename='quote_files'
+filelines=`cat $filename`
+directory=taq.12.2014/
+
+for line in $filelines ; do
+   echo $line
+   locate_dir=$'s3://'$directory$line
+   sudo aws s3 cp $locate_dir ./temp.zip;
+   name_file="$(unzip -l temp.zip | awk '/-----/ {p = ++p % 2; next} p {print $NF}')";
+   unzip temp.zip;
+   rm -f temp.zip;
+   head -n 1000 $name_file  > tmp_file;
+   mv tmp_file $name_file
+done
---
+++ hw1/modify_quote_files.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+filename='quote_files_names'
+filelines=`cat $filename`
+
+for line in $filelines ; do
+   date=${line: -8};
+   sed "s/.$/,$date/" $line | sed 's/  */-/g' | sed 's/-/,/' > tmp
+   sed "s/-,$date$/,$date/" tmp | sed '1 s/^.*$/A,B,Date/' > $line'_modified'
+done
+
+rm tmp
