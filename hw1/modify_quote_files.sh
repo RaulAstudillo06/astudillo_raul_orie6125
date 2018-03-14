b95d3e2dfea1b19c87814e3a709e8bb4011c946e
RaulAstudillo06 <astudillo0606@gmail.com>
2018-03-14 13:20:39 -0400

    change

--- hw1/modify_quote_files.sh
+++ hw1/modify_quote_files.sh
@@ -6,7 +6,7 @@ filelines=`cat $filename`
 for line in $filelines ; do
    date=${line: -8};
    sed "s/.$/,$date/" $line | sed 's/  */-/g' | sed 's/-/,/' > tmp
-   sed "s/-,$date$/,$date/" tmp | sed '1 s/^.*$/A,B,Date/' > $line'_modified'
+   sed "s/-,$date$/,$date/" tmp | sed '1 s/^.*$/A,B,Date/' > $line
 done
 
 rm tmp
